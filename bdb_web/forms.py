2fb7a87bb3a223b3860a6e12f3a19d53fdb6867a
touwwouter <woutertouw@hotmail.com>
2014-04-11 17:31:46 +0200

    Change POST to GET search (#2)

--- bdb_web/forms.py
+++ bdb_web/forms.py
@@ -1,10 +1,8 @@
-from flask_wtf import Form
-from wtforms import TextField
-from wtforms.validators import Regexp
+from wtforms import Form, TextField, validators
 
 from bdb_data import PDB_ID_PAT
 
 class SearchForm(Form):
     message = "Please provide a valid PDB identifier."
-    name = TextField("search", validators=[Regexp(regex=PDB_ID_PAT,
-                                                  message=message)])
+    name = TextField("Search a BDB entry",
+            [validators.Regexp(regex=PDB_ID_PAT, message=message)])
