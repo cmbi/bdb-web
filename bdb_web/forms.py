2a1dea069f0a06a55f461ca1ecd8d8a6ec3db224
touwwouter <woutertouw@hotmail.com>
2014-04-18 08:32:00 +0200

    Provide search tooltip for IE.

--- bdb_web/forms.py
+++ bdb_web/forms.py
@@ -11,4 +11,5 @@ class SearchForm(Form):
             label="Search a BDB entry",
             validators = [Regexp(regex=PDB_ID_PAT, message=message)],
             description = {"size": 15, "results": 5, "required": "required",
-                "placeholder": "Enter a PDB code", "pattern": PDB_ID_PAT})
+                "placeholder": "Enter a PDB code", "pattern": PDB_ID_PAT,
+                "title": message})
