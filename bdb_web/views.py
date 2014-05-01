dfdbf374d116d8d0a9d7741deff492ae7063ccec
touwwouter <woutertouw@hotmail.com>
2014-05-01 12:12:27 +0200

    Fixed 404 handling.

--- bdb_web/views.py
+++ bdb_web/views.py
@@ -41,7 +41,7 @@ def index():
     return render_template("index.html")
 
 
-@app.errorhandler("404")
+@app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
 
