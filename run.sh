db70aa01f421a32d566fe3f28cca34ee77dc2741
touwwouter <woutertouw@hotmail.com>
2014-05-06 15:08:12 +0200

    Change gunicorn appname (required for dummy-app)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export BDB_WEB_SETTINGS=~/projects/bdb-web/bdb_web_settings.cfg
-gunicorn -k gevent -b 0.0.0.0:5000 --debug bdb_web:app
+gunicorn -k gevent -b 0.0.0.0:5000 --debug bdb_web:application
