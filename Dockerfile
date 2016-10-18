df730f73bc42748e83d0c1f16c875336976d0c8b
Jon Black <jon_black@mm.st>
2016-10-18 15:12:58 +0200

    Update docker

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-FROM python:2.7
+FROM python:2.7-onbuild
 
 RUN apt-get update
 RUN apt-get install -y libfreetype6-dev pandoc
 
-RUN mkdir -p /app
-WORKDIR /app
-
-COPY requirements /app/requirements
-RUN pip install -r requirements
-COPY . /app
-
+WORKDIR /usr/src/app
 
 EXPOSE 16000
-ENV BDB_WEB_SETTINGS /app/config/settings.example
 
 CMD ["gunicorn", "-k", "gevent", "-b", "0.0.0.0:16000", "bdb_web:app"]
---
+++ prd_settings.py
@@ -0,0 +1,11 @@
+SECRET_KEY = "2k9zklk%$t)=4=qnl2+c9b$-9b2s2$d*$iy7!sau7k&knl3k$t"
+
+MAIL_SERVER = "smtp.umcn.nl"
+MAIL_SMTP_PORT = 25
+MAIL_FROM = "bdb-web_error@example.com"
+MAIL_TO = ["Jon.Black@radboudumc.nl", "Coos.Baakman@radboudumc.nl"]
+
+BDB_ROOT = "/data/bdb"
+PDB_ROOT = "/data/pdb"
+PDB_SEARCH_URL = "http://www.rcsb.org/pdb/explore/explore.do?structureId="
+WHY_NOT_SEARCH_URL = "http://www.cmbi.ru.nl/WHY_NOT2/search/pdbid/"
